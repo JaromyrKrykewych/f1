547c50a880c6aebc7b46e4efe1ccd3b7beb3af72
Jaromyr <krykewychjaromyr@gmail.com>
2025-03-20 11:56:38 -0300

    preparation to a great update on Standings

--- app/standings/page.js
+++ app/standings/page.js
@@ -29,82 +29,15 @@ const OverallStandings = () => {
 
   return (
     <div className="p-6 min-h-screen w-full bg-gradient-to-br from-gray-900 via-gray-700 to-gray-900">
-      <h1 className="text-2xl font-bold mb-4">Overall Standings</h1>
-
       {!isLoaded ? (
         <F1Loading />
       ) : (
-        // <div className="w-[80%] m-auto grid grid-cols-2 gap-4">
-        //   <div className="flex-col mr-auto">
-        //     <h2 className="text-md font-semibold mb-2">Ranking by points</h2>
-        //     <table className="min-w-1/3 border-collapse border border-gray-300">
-        //       <thead>
-        //         <tr className="bg-red-700">
-        //           <th className="border border-gray-600 px-2 py-2">Pos.</th>
-        //           <th className="border border-gray-600 px-3 py-2">Driver</th>
-        //           <th className="border border-gray-600 px-3 py-2">N° R.</th>
-        //           <th className="border border-gray-600 px-3 py-2">Wins</th>
-        //           <th className="border border-gray-600 px-3 py-2">Pts</th>
-        //         </tr>
-        //       </thead>
-
-        //       <tbody>
-        //         {standings
-        //           .sort((a, b) => {
-        //             return b.points - a.points || b.wins - a.wins;
-        //           })
-        //           .map((entry) => (
-        //             <tr
-        //               key={entry.driver}
-        //               className="text-center border border-gray-600"
-        //             >
-        //               <td className="border px-2 py-2">{entry.position}</td>
-        //               <td className="border px-3 py-2 text-left">
-        //                 {entry.driver}
-        //               </td>
-        //               <td className="border px-3 py-2">{entry.races}</td>
-        //               <td className="border px-3 py-2">{entry.wins}</td>
-        //               <td className="border px-3 py-2 font-bold">
-        //                 {entry.points}
-        //               </td>
-        //             </tr>
-        //           ))}
-        //       </tbody>
-        //     </table>
-        //   </div>
-
-        //   <div className="flex-col ml-auto">
-        //     <h2 className="text-md font-semibold mb-2">Scuderia Standings</h2>
-        //     <table className="w-full border-collapse border border-gray-600">
-        //       <thead>
-        //         <tr className="bg-red-700">
-        //           <th className="border border-gray-600 px-2 py-2">Pos.</th>
-        //           <th className="border border-gray-600 px-3 py-2 text-left">
-        //             Scuderia
-        //           </th>
-        //           <th className="border border-gray-600 px-3 py-2">Wins</th>
-        //           <th className="border border-gray-600 px-3 py-2">Pts</th>
-        //         </tr>
-        //       </thead>
-        //       <tbody>
-        //         {scuderiaStandings.map((entry) => (
-        //           <tr
-        //             key={entry.scuderia}
-        //             className="text-center border border-gray-600"
-        //           >
-        //             <td className={`border px-2 py-2`}>{entry.position}</td>
-        //             <td className={`border px-3 py-2`}>{entry.scuderia}</td>
-        //             <td className={`border px-3 py-2`}>{entry.wins}</td>
-        //             <td className={`border px-3 py-2`}>{entry.points}</td>
-        //           </tr>
-        //         ))}
-        //       </tbody>
-        //     </table>
-        //   </div>
-        // </div>
-        <div className="flex flex-col md:flex-row justify-space-between gap-12 md:gap-8">
+        <div className="mt-12 flex flex-col md:flex-row justify-space-between gap-12 md:gap-8">
           {/* Drivers */}
           <div className="w-full max-w-md mx-auto">
+            <h2 className="text-white text-xl font-bold mb-5">
+              Driver's Standing
+            </h2>
             {/* Encabezado */}
             <div className="grid grid-cols-[15%_40%_15%_15%_15%] bg-gray-700 text-white font-bold py-3 px-4 rounded-t-2xl">
               <span>Pos.</span>
@@ -145,6 +78,9 @@ const OverallStandings = () => {
           </div>
           {/* Scuderias */}
           <div className="w-full max-w-sm mx-auto">
+            <h2 className="text-white text-xl font-bold mb-5">
+              Scuderia's Standing
+            </h2>
             {/* Encabezado */}
             <div className="grid grid-cols-[15%_55%_15%_15%] bg-gray-700 text-white font-bold py-3 px-4 rounded-t-2xl">
               <span>Pos.</span>
