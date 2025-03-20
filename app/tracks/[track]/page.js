88f5ced1fbd1d05a0be232f8172fa658379a4eb2
Jaromyr <krykewychjaromyr@gmail.com>
2025-03-20 17:16:08 -0300

    some standings table updated

--- app/tracks/[track]/page.js
+++ app/tracks/[track]/page.js
@@ -48,114 +48,135 @@ const TrackData = () => {
       {!isLoaded ? (
         <F1Loading />
       ) : (
-        <div className="grid grid-cols-3">
+        <div className="w-[95%] m-auto grid grid-cols-[35%_26%_35%] gap-8">
           {/* Tabla por puntos */}
-          <div className="flex-col m-auto">
-            <h2 className="text-md font-semibold mb-2">Ranking by points</h2>
-            <table className="w-full border-collapse border border-gray-300">
-              <thead>
-                <tr className="bg-red-700">
-                  <th className="border border-gray-600 px-2 py-2">Pos.</th>
-                  <th className="border border-gray-600 px-3 py-2">Driver</th>
-                  <th className="border border-gray-600 px-3 py-2">N° R.</th>
-                  <th className="border border-gray-600 px-3 py-2">Wins</th>
-                  <th className="border border-gray-600 px-3 py-2">Pts</th>
-                </tr>
-              </thead>
+          <div className={`w-full max-w-md mx-auto`}>
+            <h2 className="text-white text-xl font-bold mb-5">
+              Ranking by points
+            </h2>
+            {/* Encabezado */}
+            <div className="grid grid-cols-[15%_40%_15%_15%_15%] bg-gray-700 text-white font-bold py-3 px-4 rounded-t-2xl">
+              <span>Pos.</span>
+              <span>Driver</span>
+              <span className="text-center">N° R.</span>
+              <span className="text-center">Wins</span>
+              <span className="text-center">Pts</span>
+            </div>
 
-              <tbody>
-                {standings
-                  .sort((a, b) => {
-                    return b.points - a.points || b.wins - a.wins;
-                  })
-                  .map((entry) => (
-                    <tr
-                      key={entry.driver}
-                      className="text-center border border-gray-600"
-                    >
-                      <td className="border px-2 py-2">{entry.position}</td>
-                      <td className="border px-3 py-2 text-left">
-                        {entry.driver}
-                      </td>
-                      <td className="border px-3 py-2">{entry.races}</td>
-                      <td className="border px-3 py-2">{entry.wins}</td>
-                      <td className="border px-3 py-2 font-bold">
-                        {entry.points}
-                      </td>
-                    </tr>
-                  ))}
-              </tbody>
-            </table>
+            {/* Filas de pilotos */}
+            <div className="flex flex-col">
+              {standings.map((entry, index) => (
+                <div
+                  key={index}
+                  className={`grid grid-cols-[15%_40%_15%_15%_15%] py-3 px-4 border-b border-gray-500 items-center
+            ${
+              index === 0
+                ? "bg-yellow-500 text-gray-900 font-bold"
+                : index === 1
+                ? "bg-gray-400 text-gray-900 font-bold"
+                : index === 2
+                ? "bg-orange-500 text-gray-900 font-bold"
+                : "bg-gradient-to-b from-gray-800 to-gray-600 text-white"
+            }
+            ${index === standings.length - 1 ? "rounded-b-2xl" : "rounded-none"}
+            `}
+                >
+                  <span className="font-bold text-lg">{entry.position}</span>
+                  <span>{entry.driver}</span>
+                  <span className="text-center">{entry.races}</span>
+                  <span className="text-center">{entry.wins}</span>
+                  <span className="font-semibold text-red-500 text-center">
+                    {entry.points}
+                  </span>
+                </div>
+              ))}
+            </div>
           </div>
 
           {/* Tabla por posición final en carrera */}
-          <div className="flex-col m-auto">
-            <h2 className="text-md font-semibold mb-2">
+          <div className={`w-full max-w-md mx-auto`}>
+            <h2 className="text-white text-xl font-bold mb-5">
               Average Final Positions
             </h2>
-            <table className="w-full border-collapse border border-gray-300">
-              <thead>
-                <tr className="bg-red-700">
-                  <th className="border border-gray-600 px-2 py-2">Pos.</th>
-                  <th className="border border-gray-600 px-3 py-2">Driver</th>
-                  <th className="border border-gray-600 px-3 py-2">Avg</th>
-                </tr>
-              </thead>
+            {/* Encabezado */}
+            <div className="grid grid-cols-[15%_65%_20%] bg-gray-700 text-white font-bold py-3 px-4 rounded-t-2xl">
+              <span>Pos.</span>
+              <span>Driver</span>
+              {/* <span className="text-center">N° R.</span> */}
+              <span className="text-center">Avg</span>
+            </div>
 
-              <tbody>
-                {positions.map((entry) => (
-                  <tr
-                    key={entry.driver}
-                    className="text-center border border-gray-600"
-                  >
-                    <td className="border px-2 py-2">{entry.position}</td>
-                    <td className="border px-3 py-2 text-left">
-                      {entry.driver}
-                    </td>
-                    <td className="border px-3 py-2">{entry.average}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+            {/* Filas de pilotos */}
+            <div className="flex flex-col">
+              {positions.map((entry, index) => (
+                <div
+                  key={index}
+                  className={`grid grid-cols-[15%_65%_20%] py-3 px-4 border-b border-gray-500 items-center
+            ${
+              index === 0
+                ? "bg-yellow-500 text-gray-900 font-bold"
+                : index === 1
+                ? "bg-gray-400 text-gray-900 font-bold"
+                : index === 2
+                ? "bg-orange-500 text-gray-900 font-bold"
+                : "bg-gradient-to-b from-gray-800 to-gray-600 text-white"
+            }
+            ${index === standings.length - 1 ? "rounded-b-2xl" : "rounded-none"}
+            `}
+                >
+                  <span className="font-bold text-lg">{entry.position}</span>
+                  <span>{entry.driver}</span>
+                  {/* <span className="text-center">{entry.races}</span> */}
+                  <span className="text-center">{entry.average}</span>
+                </div>
+              ))}
+            </div>
           </div>
 
           {/* Tabla de promedios de las 3 mejores posiciones */}
-          <div className="flex-col m-auto">
-            <h2 className="text-md font-semibold mb-2">
-              Average Best Positions
+          <div className={`w-full max-w-md mx-auto`}>
+            <h2 className="text-white text-xl font-bold mb-5">
+              Best Perfomances Average
             </h2>
-            <table className="w-full border-collapse border border-gray-300">
-              <thead>
-                <tr className="bg-red-700">
-                  <th className="border border-gray-600 px-2 py-2">Pos.</th>
-                  <th className="border border-gray-600 px-3 py-2">Driver</th>
-                  <th className="border border-gray-600 px-3 py-2">Avg</th>
-                  <th className="border border-gray-600 px-3 py-2">1°</th>
-                  <th className="border border-gray-600 px-3 py-2">2°</th>
-                  <th className="border border-gray-600 px-3 py-2">3°</th>
-                </tr>
-              </thead>
+            {/* Encabezado */}
+            <div className="grid grid-cols-[12%_40%_12%_12%_12%_12%] bg-gray-700 text-white font-bold py-3 px-4 rounded-t-2xl">
+              <span>Pos.</span>
+              <span>Driver</span>
+              <span className="text-center">Avg</span>
+              <span className="text-center">1°</span>
+              <span className="text-center">2°</span>
+              <span className="text-center">3°</span>
+            </div>
 
-              <tbody>
-                {bestAverages.map((entry) => (
-                  <tr
-                    key={entry.driver}
-                    className="text-center border border-gray-600"
-                  >
-                    <td className="border px-2 py-2">{entry.position}</td>
-                    <td className="border px-3 py-2 text-left">
-                      {entry.driver}
-                    </td>
-                    <td className="border px-3 py-2 font-bold">
-                      {entry.average}
-                    </td>
-                    <td className="border px-3 py-2">{entry.best}</td>
-                    <td className="border px-3 py-2">{entry.second}</td>
-                    <td className="border px-3 py-2">{entry.third}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+            {/* Filas de pilotos */}
+            <div className="flex flex-col">
+              {bestAverages.map((entry, index) => (
+                <div
+                  key={index}
+                  className={`grid grid-cols-[12%_40%_12%_12%_12%_12%] py-3 px-4 border-b border-gray-500 items-center
+            ${
+              index === 0
+                ? "bg-yellow-500 text-gray-900 font-bold"
+                : index === 1
+                ? "bg-gray-400 text-gray-900 font-bold"
+                : index === 2
+                ? "bg-orange-500 text-gray-900 font-bold"
+                : "bg-gradient-to-b from-gray-800 to-gray-600 text-white"
+            }
+            ${index === standings.length - 1 ? "rounded-b-2xl" : "rounded-none"}
+            `}
+                >
+                  <span className="font-bold text-lg">{entry.position}</span>
+                  <span>{entry.driver}</span>
+                  <span className="font-semibold text-red-500 text-center">
+                    {entry.average}
+                  </span>
+                  <span className="text-center">{entry.best}</span>
+                  <span className="text-center">{entry.second}</span>
+                  <span className="text-center">{entry.third}</span>
+                </div>
+              ))}
+            </div>
           </div>
 
           {/* Tabla de promedios final */}
@@ -202,120 +223,6 @@ const TrackData = () => {
             </table>
           </div>
 
-          {/* Tabla de vueltas lideradas */}
-          {/* <div className="flex-col m-auto mt-12">
-          <h2 className="text-md font-semibold mb-2">Laps Led Standings</h2>
-          <table className="w-full border-collapse border border-gray-300">
-            <thead>
-              <tr className="bg-gray-200 dark:bg-blue-600">
-                <th className="border border-gray-300 dark:border-blue-800 px-3 py-2">
-                  Pos.
-                </th>
-                <th className="border border-gray-300 dark:border-blue-800 px-3 py-2">
-                  Driver
-                </th>
-                <th className="border border-gray-300 dark:border-blue-800 px-3 py-2">
-                  Laps
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {lapsLedStandings.slice(0, 20).map((entry) => (
-                <tr
-                  key={entry.driver}
-                  className="text-center border border-gray-300"
-                >
-                  <td
-                    className={`border border-gray-300 ${
-                      entry.position < 11 ? "bg-blue-500" : ""
-                    } px-3 py-2`}
-                  >
-                    {entry.position}
-                  </td>
-                  <td
-                    className={`border border-gray-300 ${
-                      entry.position < 11 ? "bg-blue-500" : ""
-                    } px-3 py-2`}
-                  >
-                    {entry.driver}
-                  </td>
-                  <td
-                    className={`border border-gray-300 ${
-                      entry.position < 11 ? "bg-blue-500" : ""
-                    } px-3 py-2`}
-                  >
-                    {entry.lapsLed}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div> */}
-
-          {/* Tabla de victorias */}
-          {/* <div className="flex-col m-auto mt-12">
-          <h2 className="text-md font-semibold mb-2">Victory Table</h2>
-          <table className="w-full border-collapse border border-gray-300">
-            <thead>
-              <tr className="bg-gray-200 dark:bg-blue-600">
-                <th className="border border-gray-300 dark:border-blue-800 px-3 py-2">
-                  Pos.
-                </th>
-                <th className="border border-gray-300 dark:border-blue-800 px-3 py-2">
-                  Driver
-                </th>
-                <th className="border border-gray-300 dark:border-blue-800 px-3 py-2">
-                  Wins
-                </th>
-                <th className="border border-gray-300 dark:border-blue-800 px-3 py-2">
-                  Pts
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {standings
-                .sort((a, b) => {
-                  return b.wins - a.wins || b.points - a.points;
-                })
-                .slice(0, 20)
-                .map((entry, index) => (
-                  <tr
-                    key={entry.driver}
-                    className="text-center border border-gray-300"
-                  >
-                    <td
-                      className={`border border-gray-300 ${
-                        index < 10 ? "bg-blue-500" : ""
-                      } px-3 py-2`}
-                    >
-                      {index + 1}
-                    </td>
-                    <td
-                      className={`border border-gray-300 ${
-                        index < 10 ? "bg-blue-500" : ""
-                      } px-3 py-2`}
-                    >
-                      {entry.driver}
-                    </td>
-                    <td
-                      className={`border border-gray-300 ${
-                        index < 10 ? "bg-blue-500" : ""
-                      } px-3 py-2`}
-                    >
-                      {entry.wins}
-                    </td>
-                    <td
-                      className={`border border-gray-300 ${
-                        index < 10 ? "bg-blue-500" : ""
-                      } px-3 py-2`}
-                    >
-                      {entry.points}
-                    </td>
-                  </tr>
-                ))}
-            </tbody>
-          </table>
-        </div> */}
           <div className="flex-col m-auto mt-12">
             {/* Tabla de posiciones de Escuderias */}
             <div className="flex-col m-auto">
